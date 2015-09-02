cadc00b2de12400a7b3dbbd3825686aba6057013
fly2melb <fly2melb@gmail.com>
2015-09-02 12:58:40 +1000

    Initial version with Two Submit Buttons demo.

---
+++ MvcDemo/Controllers/DemoController.cs
@@ -0,0 +1,37 @@
+﻿using MvcDemo.Helpers;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        #region "Two submit buttons"
+
+        public ViewResult TwoSubmitButtons()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost, ValidateAntiForgeryToken]
+        [ActionName("TwoSubmitButtons"), AcceptButtonName(Name = "Accept")]
+        public ViewResult Accept(string message)
+        {
+            ViewBag.Response = "Accepted";
+
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost, ValidateAntiForgeryToken]
+        [ActionName("TwoSubmitButtons"), AcceptButtonName(Name = "Decline")]
+        public ViewResult Decline(string message)
+        {
+            ViewBag.Response = "Declined";
+
+            return View();
+        }
+
+        #endregion // Two submit buttons
+    }
+}
---
+++ MvcDemo/Helpers/AcceptButtonNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace MvcDemo.Helpers
+{
+    public class AcceptButtonNameAttribute : ActionNameSelectorAttribute
+    {
+        public string Name { get; set; }
+
+        public override bool IsValidName(ControllerContext controllerContext, string actionName, MethodInfo methodInfo)
+        {
+            return controllerContext.Controller.ValueProvider.GetValue(Name) != null;
+        }
+    }
+}
