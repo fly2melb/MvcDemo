c96f41bd5f4f7691f17442cdf0afa52186e45c98
fly2melb <fly2melb@gmail.com>
2015-09-03 14:53:51 +1000

    Add demo of cascading drop-down list.

--- MvcDemo/Controllers/DemoController.cs
+++ MvcDemo/Controllers/DemoController.cs
@@ -1,4 +1,7 @@
 ﻿using MvcDemo.Helpers;
+using MvcDemo.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace MvcDemo.Controllers
@@ -31,5 +34,115 @@ namespace MvcDemo.Controllers
         }
 
         #endregion // Two submit buttons
+
+        #region "Cascading drop-down list"
+
+        public ViewResult CascadingDropDown()
+        {
+            var viewModel = new CascadingDropDownViewModel();
+            viewModel.ContinentList = GetContinents();
+
+            return View(viewModel);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ViewResult CascadingDropDown(CascadingDropDownViewModel viewModel)
+        {
+            if (viewModel.ContinentId != (int)Continent.Antarctica && viewModel.CountryId == null)
+            {
+                ModelState.AddModelError("CountryId", "Please select a country.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = "Selected";
+            }
+
+            viewModel.ContinentList = GetContinents();
+            viewModel.CountryList = GetCountries(viewModel.ContinentId);
+
+            return View(viewModel);
+        }
+
+        public JsonResult Countries(int? id)
+        {
+            if (HttpContext.Request.IsAjaxRequest())
+            {
+                return Json(new SelectList(GetCountries(id), "Key", "Value"), JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new object { });
+            }
+        }
+
+        private enum Continent
+        {
+            Africa = 1,
+            Antarctica = 2,
+            Asia = 3,
+            Europe = 4,
+            NorthAmerica = 5,
+            Oceania = 6,
+            SouthAmerica = 7
+        }
+
+        /// <summary>
+        /// Gets list of continents.
+        /// </summary>
+        /// <returns></returns>
+        private List<KeyValuePair<int, string>> GetContinents()
+        {
+            var list = new List<KeyValuePair<int, string>>();
+
+            foreach (var value in Enum.GetValues(typeof(Continent)))
+            {
+                list.Add(new KeyValuePair<int, string>((int)value, value.ToString()));
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Gets list of countries with specified continent ID.
+        /// </summary>
+        /// <param name="id">Continent ID.</param>
+        /// <returns></returns>
+        private List<KeyValuePair<int, string>> GetCountries(int? id)
+        {
+            var list = new List<KeyValuePair<int, string>>();
+
+            if (id == (int)Continent.Africa)
+            {
+                list.Add(new KeyValuePair<int, string>(1, "Algeria"));
+                list.Add(new KeyValuePair<int, string>(2, "Angola"));
+            }
+            else if (id == (int)Continent.Asia)
+            {
+                list.Add(new KeyValuePair<int, string>(3, "Afghanistan"));
+            }
+            else if (id == (int)Continent.Europe)
+            {
+                list.Add(new KeyValuePair<int, string>(4, "Albania"));
+                list.Add(new KeyValuePair<int, string>(5, "Andorra"));
+                list.Add(new KeyValuePair<int, string>(6, "Austria"));
+            }
+            else if (id == (int)Continent.NorthAmerica)
+            {
+                list.Add(new KeyValuePair<int, string>(7, "Antigua and Barbuda"));
+            }
+            else if (id == (int)Continent.Oceania)
+            {
+                list.Add(new KeyValuePair<int, string>(8, "Australia"));
+            }
+            else if (id == (int)Continent.SouthAmerica)
+            {
+                list.Add(new KeyValuePair<int, string>(9, "Argentina"));
+            }
+
+            return list;
+        }
+
+        #endregion // Cascading drop-down list
     }
 }
---
+++ MvcDemo/ViewModels/CascadingDropDownViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo.ViewModels
+{
+    public class CascadingDropDownViewModel
+    {
+        public CascadingDropDownViewModel()
+        {
+            ContinentList = new List<KeyValuePair<int, string>>();
+            CountryList = new List<KeyValuePair<int, string>>();
+        }
+
+        public List<KeyValuePair<int, string>> ContinentList { get; set; }
+
+        public List<KeyValuePair<int, string>> CountryList { get; set; }
+
+        [Required(ErrorMessage = "Please select a continent.")]
+        [Display(Name = "Continent")]
+        public int? ContinentId { get; set; }
+
+        [Display(Name = "Country")]
+        public int? CountryId { get; set; }
+    }
+}
